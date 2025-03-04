1a77e4628861386220d8aaa69352bfef5bb4acf7
Xu Shuyao <timsiu6@gmail.com>
2025-03-04 17:12:31 +0800

    feat: Improve main_generation.py to track log_prob

--- verl/verl/trainer/main_generation.py
+++ verl/verl/trainer/main_generation.py
@@ -65,18 +65,31 @@ def main(config):
     wg.init_model()
 
     total_samples = len(dataset)
+    print(f"Total samples: {total_samples}")
     # real_batch_size = data.batch['input_ids'].shape[0]
     config_batch_size = config.data.batch_size
     dp_size = wg.world_size // config.rollout.tensor_model_parallel_size
+    print(f"DP size: {dp_size}")
     num_batch = (total_samples // config_batch_size) + 1
-    output_lst = [[] for _ in range(config.data.n_samples)]
+    # output_lst = [[] for _ in range(config.data.n_samples)]
 
+    makedirs(config.data.output_dir, exist_ok=True)
     for batch_idx in range(num_batch):
         print(f'[{batch_idx+1}/{num_batch}] Start to process.')
-        batch_chat_lst = chat_lst[batch_idx * config_batch_size:(batch_idx + 1) * config_batch_size]
+        start_index = batch_idx * config_batch_size
+        end_index = min((batch_idx + 1) * config_batch_size, total_samples)
+        if os.path.exists(os.path.join(config.data.output_path, f"partition_{start_index}_{end_index}.parquet")):
+            print(f'Batch {batch_idx}, from {start_index} to {end_index} already exists, skipping.')
+            continue
+        
+        df_sub = dataset.iloc[start_index:end_index, :].copy(deep=True)
+        batch_chat_lst = chat_lst[start_index:end_index]
+        if not batch_chat_lst:
+            break
+        # batch_chat_lst = chat_lst[batch_idx * config_batch_size:(batch_idx + 1) * config_batch_size]
         inputs = tokenizer.apply_chat_template(batch_chat_lst,
                                                add_generation_prompt=True,
-                                               padding=True,
+                                               padding="max_length", # pad to max_length instead of max_length of the batch
                                                truncation=True,
                                                max_length=config.rollout.prompt_length,
                                                return_tensors='pt',
@@ -103,35 +116,89 @@ def main(config):
 
         print(f'[{batch_idx+1}/{num_batch}] Start to generate.')
         # START TO GENERATE FOR n_samples TIMES
-        for i in range(config.data.n_samples):
-            output = wg.generate_sequences(data)
-            # remove dummy data
-            output = output[:real_batch_size]
-            output_text = tokenizer.batch_decode(output.batch['input_ids'][:, -config.rollout.response_length:],
+        # for i in range(config.data.n_samples):
+        #     output = wg.generate_sequences(data)
+        output = wg.generate_sequences(data)
+        rollout_n = config.rollout.n
+        max_length = config.rollout.response_length
+        assert output.batch.batch_size[0] == batch_size * rollout_n, f'output batch size {output.batch.batch_size[0]} != {batch_size} * {rollout_n}'
+
+        responses = output.batch["responses"].reshape(batch_size, rollout_n, -1)
+        print(f"Response shape: {responses.shape}")
+        responses_lst = responses.tolist()
+        df_sub["raw_responses"] = responses_lst
+
+        input_ids = output.batch["input_ids"].reshape(batch_size, rollout_n, -1)
+        print(f"Input IDs shape: {input_ids.shape}")
+        input_ids = input_ids.tolist()
+        df_sub["input_ids"] = input_ids
+
+        attention_mask = output.batch["attention_mask"].reshape(batch_size, rollout_n, -1)
+        print(f"Attention mask shape: {attention_mask.shape}")
+        attention_mask = attention_mask.tolist()
+        df_sub["attention_mask"] = attention_mask
+
+        position_ids = output.batch["position_ids"].reshape(batch_size, rollout_n, -1)
+        print(f"Position IDs shape: {position_ids.shape}")
+        position_ids = position_ids.tolist()
+        df_sub["position_ids"] = position_ids
+
+
+        old_log_probs = output.batch["old_log_probs"].reshape(batch_size, rollout_n, -1)
+        # print(old_log_probs.shape)
+        old_log_probs_lst = old_log_probs.tolist()
+        df_sub["old_log_probs"] = old_log_probs_lst
+        def reshape_list(input_list, batch_size, rollout_n):
+            batched_list = []
+            index = 0
+            for _ in range(batch_size):
+                batch = []
+                for _ in range(rollout_n):
+                    if index < len(input_list):
+                        batch.append(input_list[index])
+                        index += 1
+                    else:
+                        break # Stop if input list is exhausted
+                batched_list.append(batch)
+            return batched_list
+
+        # output = output[:real_batch_size]
+        output_text = tokenizer.batch_decode(output.batch['input_ids'][:, -config.rollout.response_length:],
                                                  skip_special_tokens=False)
 
-            # remove the padding
-            pad_token = tokenizer.pad_token
-            output_text_unpad = []
-            for text in output_text:
-                output_text_unpad.append(text.replace(pad_token, ''))
+        # remove the padding
+        pad_token = tokenizer.pad_token
+        output_text_unpad = []
+        for text in output_text:
+            output_text_unpad.append(text.replace(pad_token, ''))
+        output_text_unpad_reshape = reshape_list(output_text_unpad, config_batch_size, rollout_n)
+        df_sub["decoded_responses"] = output_text_unpad_reshape
+        output_file = os.path.join(config.data.output_dir, f"partition_{start_index}_{end_index}.parquet")
+        df_sub.to_parquet(output_file)
 
-            output_lst[i].extend(output_text_unpad)
+        # output_lst[i].extend(output_text_unpad)
 
     # convert output_lst from (n_samples, n_data) to (n_data, n_sampels)
-    output_lst = np.array(output_lst, dtype=object)
-    output_lst = np.transpose(output_lst, axes=(1, 0)).tolist()
+    # output_lst = np.array(output_lst, dtype=object)
+    # output_lst = np.transpose(output_lst, axes=(1, 0)).tolist()
 
     # add to the data frame
-    dataset[f'responses'] = output_lst
+    # dataset[f'responses'] = output_lst
 
     # write to a new parquet
+    # output_dir = os.path.dirname(config.data.output_path)
+    # makedirs(output_dir, exist_ok=True)
+    # dataset.to_parquet(config.data.output_path)
+
+    # return output_text
+    # Collect all sub parquet files
+    sub_files = [os.path.join(config.data.output_dir, f) for f in os.listdir(config.data.output_dir) if f.endswith('.parquet')]
+    # Concatenate all sub parquet files
+    combined_df = pd.concat([pd.read_parquet(f) for f in sub_files], ignore_index=True)
+    # Write the combined dataframe to a new parquet file
     output_dir = os.path.dirname(config.data.output_path)
     makedirs(output_dir, exist_ok=True)
-    dataset.to_parquet(config.data.output_path)
-
-    return output_text
-
+    combined_df.to_parquet(config.data.output_path)
 
 if __name__ == '__main__':
     main()
